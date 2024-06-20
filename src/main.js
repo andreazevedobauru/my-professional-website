5071e04d80e1df7d52a6d1b8020fb184640bca9d
André Felipe Azevedo <andre.felipe.az@gmail.com>
2024-06-20 17:29:59 -0300

    Added google analitics

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
+import VueGtag from 'vue-gtag'
 
 import 'animate.css'
 import './assets/css/tailwind.css'
@@ -10,5 +11,8 @@ import 'aos/dist/aos.css'
 
 const app = createApp(App)
 app.use(router)
+app.use(VueGtag, {
+  config: { id: 'G-18BTF534KT' }
+}, router)
 app.use(i18n)
 app.mount('#app')
